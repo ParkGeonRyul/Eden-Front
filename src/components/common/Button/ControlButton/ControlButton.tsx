a9177d6e5cd9372cdc5b2ee1154995810d476f3b
wanwork <wjdqo9705@gmail.com>
2024-01-08 18:22:31 +0900

    ✨feat: add label component & modify control button component

--- src/components/common/Button/ControlButton/ControlButton.tsx
+++ src/components/common/Button/ControlButton/ControlButton.tsx
@@ -2,11 +2,14 @@ import * as S from '@/components/common/Button/ControlButton/controlButton.style
 
 interface ButtonProps {
   type: 'edit' | 'delete';
-  children: string;
 }
 
-const ControlButton: React.FC<ButtonProps> = ({ type, children }) => {
-  return <S.StyledButton type={type}>{children}</S.StyledButton>;
+const ControlButton: React.FC<ButtonProps> = ({ type }) => {
+  return (
+    <S.StyledButton type={type}>
+      {type === 'edit' ? '수정' : '삭제'}
+    </S.StyledButton>
+  );
 };
 
 export default ControlButton;
