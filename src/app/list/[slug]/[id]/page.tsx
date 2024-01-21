aa06b0c4e34d6f93aff7c3862ae438700ad93766
wanwork <wjdqo9705@gmail.com>
2024-01-22 03:57:09 +0900

    ✨ feat: list page publishing

---
+++ src/app/list/[slug]/[id]/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { ListItem } from '@/types/apis/listItem';
+import PageTitle from '@/components/PageTitle/PageTitle';
+import * as S from './listDetail.style';
+
+type Props = {
+  params: {
+    slug: string;
+    id: string;
+  };
+};
+
+export default function ListDetail({ params }: Props) {
+  const [listItem, setListItem] = useState<ListItem>({
+    postId: 0,
+    title: '',
+    content: '',
+    date: '',
+    //image data 필요.
+  });
+
+  useEffect(() => {
+    //TODO api 분리 후 id에 따라 다이나믹 통신하기!
+    // 백엔드랑 통신시 api 주소 및 양식 변경 예정
+    fetch(`/mock/listDetail.json`)
+      .then((res) => res.json())
+      .then((result: { data: ListItem }) => setListItem(result.data));
+  }, []);
+
+  return (
+    <>
+      <PageTitle>{params.slug.toUpperCase()}</PageTitle>
+      <S.InquiryDetailContainer>
+        <S.HeaderContainer>
+          <S.InquiryHeader>
+            <S.InquiryTitle>{listItem.title}</S.InquiryTitle>
+            <S.InquiryInfo>
+              <S.Date>{listItem.date}</S.Date>
+            </S.InquiryInfo>
+          </S.InquiryHeader>
+        </S.HeaderContainer>
+
+        <S.ImageContainer></S.ImageContainer>
+
+        <S.InquiryBody>{listItem.content}</S.InquiryBody>
+      </S.InquiryDetailContainer>
+    </>
+  );
+}
---
+++ src/components/ListPage/PageButton/PageButton.tsx
@@ -0,0 +1,12 @@
+import { ListPageButton, PaginationContainer } from './pageButton.style';
+
+function PageButton() {
+  //TODO 페이지네이션 조율 후 변경 예정
+  return (
+    <PaginationContainer>
+      <ListPageButton>1</ListPageButton>
+    </PaginationContainer>
+  );
+}
+
+export default PageButton;
