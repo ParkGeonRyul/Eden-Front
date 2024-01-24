9055a4fc3261102e5206ab8eefb881eb879c69eb
인님 <innim_e@jeonhyeongmin-ui-MacBookAir.local>
2024-01-24 16:42:42 +0900

    🐛 fix      : Admin페이지 UI완료

---
+++ src/app/admin/admin.style.ts
@@ -0,0 +1,75 @@
+'use client';
+import styled from 'styled-components';
+
+export const Category = styled.div`
+  width: 100%;
+  margin-top: 90px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const Table = styled.div`
+  width: 500px;
+  margin-bottom: 60px;
+`;
+
+export const TableHeader = styled.div`
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderName = styled.div`
+  font-size: 50px;
+  font-weight: 300;
+`;
+
+export const MoreButton = styled.button`
+  width: 90px;
+  height: 45px;
+  background-color: #e4d5c2;
+  font-size: 20px;
+`;
+
+export const TableBody = styled.div`
+  border: 1px solid #e6e6e6;
+  border-bottom: none;
+  font-size: 17px;
+`;
+
+export const BodyStyle = styled.div`
+  height: 60px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+`;
+export const NumberBox = styled.div`
+  width: 80px;
+  padding: 0 10px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const TitleBox = styled.div`
+  width: 300px;
+  padding: 0 10px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const StateContainer = styled.div`
+  width: 120px;
+  text-align: center;
+`;
+
+export const AnswerState = styled.div``;
+
+export const LabelBox = styled.div``;
