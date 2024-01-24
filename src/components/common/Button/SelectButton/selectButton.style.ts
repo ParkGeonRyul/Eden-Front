98784315543f320394148c9939925ff6f8a92f4d
wanwork <wjdqo9705@gmail.com>
2024-01-25 01:29:47 +0900

    🚑 hotfix: add onClick props in button component

--- src/components/common/Button/SelectButton/selectButton.style.ts
+++ src/components/common/Button/SelectButton/selectButton.style.ts
@@ -7,7 +7,18 @@ interface ButtonProps {
   children: string;
 }
 
-const commonStyles = css`
+const nonSelectedStyles = css`
+  border: 1px solid #e0e0e0;
+  color: #616161;
+  background: #f7f9fb;
+`;
+
+const selectedStyles = css`
+  color: #fff;
+  background: #5d8554;
+`;
+
+export const StyledButton = styled.button<ButtonProps>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -21,20 +32,5 @@ const commonStyles = css`
   letter-spacing: -0.5px;
 
   cursor: pointer;
-`;
-
-const nonSelectedStyles = css`
-  border: 1px solid #e0e0e0;
-  color: #616161;
-  background: #f7f9fb;
-`;
-
-const selectedStyles = css`
-  color: #fff;
-  background: #5d8554;
-`;
-
-export const StyledButton = styled.button<ButtonProps>`
-  ${commonStyles}
   ${({ isSelected }) => (isSelected ? selectedStyles : nonSelectedStyles)}
 `;
