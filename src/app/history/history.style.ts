a9b5d256dec545ffb673d57f7a1757fe21812c52
인님 <115972123+InnimE@users.noreply.github.com>
2024-01-29 18:11:01 +0900

    ✨ feat     : History(연혁) 페이지 UI 완료 (#21)
    Co-authored-by: 인님 <innim_e@jeonhyeongmin-ui-MacBookAir.local>

---
+++ src/app/history/history.style.ts
@@ -0,0 +1,54 @@
+'use client';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+export const SubTitle = styled.div`
+  text-align: center;
+  margin: 30px auto 55px;
+`;
+
+export const ContainerLine = styled.div`
+  border-top: 1px solid black;
+`;
+
+export const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 95px;
+  display: flex;
+  gap: 70px;
+`;
+
+export const ImgContainer = styled.div``;
+
+export const StyleImage = styled(Image)``;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InfoStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+`;
+
+export const Head = styled.div`
+  font-size: 32px;
+`;
+export const Name = styled.div`
+  margin-bottom: 15px;
+  font-size: 24px;
+`;
+export const Info = styled.span`
+  font-size: 16px;
+`;
+export const Additional = styled.div`
+  margin: 20px 0;
+`;
+
+export const Bold = styled.span`
+  font-size: 16px;
+  font-weight: 700;
+`;
---
+++ src/app/history/page.tsx
@@ -0,0 +1,71 @@
+'use client';
+import * as S from './history.style';
+import Mainpage from '@/components/MainPage/MainPage';
+import PageTitle from '@/components/PageTitle/PageTitle';
+
+interface infoItems {
+  infoItems: string;
+}
+
+interface addtionalInfo {
+  boldStyle: string;
+  addInfo: string;
+}
+
+const History = () => {
+  const infoItems: infoItems[] = [
+    { infoItems: 'Professor, Department of Psychology, Sogang University' },
+    { infoItems: 'University of Minnesota, Twin Cities, Ph.D.' },
+    { infoItems: 'Yonsei University, M.A.' },
+    { infoItems: 'Yonsei University, B.S.' },
+    { infoItems: '(Pronouns Used: He/him/his)' },
+  ];
+
+  const addtionalItems: addtionalInfo[] = [
+    {
+      boldStyle: 'Specialties:',
+      addInfo:
+        ' Depression, Anxiety, Relationship Issues, Identity Issues, Career Issues, and Cross-cultural',
+    },
+    {
+      boldStyle: 'Theories Used:',
+      addInfo:
+        ' Feedback-Informed Treatment, Person-Centered Theory, Cognitive-Behavioral',
+    },
+  ];
+
+  return (
+    <Mainpage>
+      <PageTitle>History</PageTitle>
+      <S.SubTitle>연혁</S.SubTitle>
+      <S.ContainerLine>
+        <S.Container>
+          <S.ImgContainer>
+            <S.StyleImage
+              src="/historySample.jpeg"
+              alt="이미지 로드에 실패했습니다."
+              width={260}
+              height={350}
+            />
+          </S.ImgContainer>
+          <S.InfoContainer>
+            <S.Head>Consulting Advisors</S.Head>
+            <S.Name>Kyujin Yon, Ph.D.</S.Name>
+            <S.InfoStyled>
+              {infoItems.map((info) => (
+                <S.Info>{info.infoItems}</S.Info>
+              ))}
+            </S.InfoStyled>
+            {addtionalItems.map((addtional) => (
+              <S.Additional>
+                <S.Bold>{addtional.boldStyle}</S.Bold>
+                <S.Info>{addtional.addInfo}</S.Info>
+              </S.Additional>
+            ))}
+          </S.InfoContainer>
+        </S.Container>
+      </S.ContainerLine>
+    </Mainpage>
+  );
+};
+export default History;
